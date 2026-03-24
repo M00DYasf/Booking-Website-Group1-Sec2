bfe832aa48b3da6470a4476ca43f255b586ae57c
Cameron <132107775+Wabathon@users.noreply.github.com>
2026-03-24 18:33:43 -0400

    Delete domain directory

--- domain/entities/Inventory.ts
+++
@@ -1,6 +0,0 @@
-export class Inventory {
-  constructor(
-    public resourceId: string,
-    public totalUnits: number // how many rooms/cars exist
-  ) {}
-}
--- domain/user.ts
+++
@@ -1,7 +0,0 @@
-export interface IUser {
-  name: string;
-  email: string;
-  password: string;
-  role: "guest" | "user" | "admin";
-  createdAt?: Date;
-}
